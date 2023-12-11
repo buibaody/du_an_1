903545eb7f13cce9f20c715975074e5b61d0c8cf
buibaody <153476523+buibaody@users.noreply.github.com>
2023-12-11 21:04:20 +0700

    Update connect.php

--- Admin/model/connect.php
+++ Admin/model/connect.php
@@ -5,7 +5,7 @@
         $password = "";
 
         try {
-        $conn = new PDO("mysql:host=$servername;dbname=asm", $username, $password);
+        $conn = new PDO("mysql:host=$servername;dbname=duannhombon", $username, $password);
         // set the PDO error mode to exception        
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // echo "Connected successfully";
